d6281ed9e0111e3980f55b876e7d4eccec57beac
saikamara59 <mhsaidu@gmail.com>
2024-11-16 06:56:40 -0500

    created a Main layout page

--- aura-tunes/src/components/Topbar.tsx
+++ aura-tunes/src/components/Topbar.tsx
@@ -8,7 +8,7 @@ const Topbar = () => {
   return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
   
-      <div className="flex gap-2 items-center">Spotify</div>
+      <div className="flex gap-2 items-center">AuraTunes</div>
       <div className="flex items-center gap-4">
         {isAdmin && (
           <Link to={"/admin"}>
