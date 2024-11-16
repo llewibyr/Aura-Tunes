9a879459356b61844dd1f67508b28385e35d0e29
saikamara59 <mhsaidu@gmail.com>
2024-11-15 22:58:55 -0500

    AuthCallBackPage.tsx

--- aura-tunes/src/App.tsx
+++ aura-tunes/src/App.tsx
@@ -1,23 +1,20 @@
 
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
-import AuthCallbackPage from "./pages/home/auth-callback/AuthCallbackPage";
-import { axiosInstance } from "./lib/axios";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+
 
 
 function App() {
-  const getSomeData = async () => {
-   const res = await axiosInstance.get("/users",{
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-   })
-    console.log(res)
-  }
+
   return (
     <>
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path='/sso-callback' element= {<AuthenticateWithRedirectCallback
+      signUpForceRedirectUrl={"/auth-callback"}
+      />}/>
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
     </Routes>
     </>
