45d0c53e18ebb8bde605dd918498bfdc4d2b8ca7
saikamara59 <mhsaidu@gmail.com>
2024-11-15 19:31:45 -0500

    routes

--- aura-tunes/src/App.tsx
+++ aura-tunes/src/App.tsx
@@ -1,17 +1,15 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import { Button } from "./components/ui/button"
+
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/home/auth-callback/AuthCallbackPage";
 
 function App() {
   return (
     <>
-     <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+    </Routes>
     </>
   );
 }
--- aura-tunes/src/main.tsx
+++ aura-tunes/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router-dom'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -14,8 +15,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
    
    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+   <BrowserRouter>
     <App />
+    </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
 )
